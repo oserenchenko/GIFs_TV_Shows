e48f11e37a62b432a1ca7962ab3237683690c6bd
Olga <oserenchenko@gmail.com>
2018-06-26 17:20:57 -0600

    form for new show name and submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 //array with intial tv shows
-var tvShows = ['The Office', 'Game of Thrones', 'Parks and Recreation', 'Friends', 'Greys Anatomy', 'Suits', 'The Walking Dead', 'Modern Family', 'Vikings', 'Black Mirror', 'Stranger Things', 'Breaking Bad']
+var tvShows = ['the office', 'game of thrones', 'parks and recreation', 'friends', 'greys anatomy', 'suits', 'the walking dead', 'modern family', 'vikings', 'black mirror', 'stranger things', 'breaking bad']
 
 function makeButtons() {
+  $('.buttons').empty();
   for (var i = 0; i < tvShows.length; i++) {
     var tvShow = tvShows[i];
     var newButton = $('<button>');
@@ -16,12 +17,12 @@ function makeButtons() {
 
 function displayGifs() {
   var tvShow = $(this).attr('showName');
-  var queryURL = 'https:/api.giphy.com/v1/gifs/search?api_key=qL0YObgRkosgTuGJFuZX2DFFSetWSrE6&limit=10&q=' + tvShow ;
+  var queryURL = 'https:/api.giphy.com/v1/gifs/search?api_key=qL0YObgRkosgTuGJFuZX2DFFSetWSrE6&limit=10&q=' + tvShow;
 
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }).then(function(response) {
+  }).then(function (response) {
     $('.gifResults').empty();
     for (var i = 0; i < response.data.length; i++) {
       var newImg = $('<img>');
@@ -35,7 +36,7 @@ function displayGifs() {
       });
       $('.gifResults').append(newImg);
     }
-    $('.gifImage').on('click', function() {
+    $('.gifImage').on('click', function () {
       var state = $(this).attr('data-state');
       if (state == 'still') {
         $(this).attr('src', $(this).attr('gif'));
@@ -44,15 +45,20 @@ function displayGifs() {
         $(this).attr('src', $(this).attr('still'));
         $(this).attr('data-state', 'still');
       }
-    });    
+    });
   })
 }
 
+$("#submitBtn").on("click", function (event) {
+  event.preventDefault();
+  var newTvShow = $("#newTvShowToAdd").val().trim();
+  if (tvShows.indexOf(newTvShow.toLowerCase()) < 0) {
+    console.log(tvShows.indexOf(newTvShow));
+    tvShows.push(newTvShow);
+    makeButtons();
+  }
+});
 
+makeButtons();
 
-
-
-
-  makeButtons();
-
-  $(document).on('click', '.showButton', displayGifs);
+$(document).on('click', '.showButton', displayGifs);
